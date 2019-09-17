8f9d304a3447751281aba67117efde1e497decde
NickBurness <Nickburness93@outlook.com>
2019-09-17 16:14:51 +0100

    this commit is for the hunger and fitness methods for virtual pet

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -10,15 +10,31 @@ describe('constructor', () => {
 describe('constructor', () => {
 
     it('has a initial age of 0', () => {
-      const pet = new Pet('Fido');
+      const pet = new Pet('Luigi');
       expect(pet.age).toEqual(0);
     });
   });
 
 describe('growUp', () => {
     it('increments the age by 1', () => {
-      const pet = new Pet('Fido');
+      const pet = new Pet('Luigi');
       pet.growUp();
       expect(pet.age).toEqual(1);
     });
   });
+
+describe('hunger', () => {
+    it('decreases hunger by 5', () => {
+        const pet = new Pet('Luigi');
+        pet.hunger();
+        expect(pet.hunger).toEqual(0);
+      });
+    });
+
+describe('fitness', () => {
+    it('decreases fitness by 3', () => {
+        const pet = new Pet('Luigi');
+        pet.fitness();
+        expect(pet.fitness).toEqual(10);
+    })
+})
