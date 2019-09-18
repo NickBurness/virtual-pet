cea3f58de33c0f800cef92cf3f1d8c3215aa7951
NickBurness <Nickburness93@outlook.com>
2019-09-18 11:20:40 +0100

    this commit includes updates to virtual pets fitness, now includes a walk(); and growUp(); function.

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -36,5 +36,23 @@ describe('fitness', () => {
         const pet = new Pet('Luigi');
         pet.fitness();
         expect(pet.fitness).toEqual(10);
-    })
-})
+    });
+});
+
+describe('walk', () => {
+    it('increases fitness by 4', () => {
+      const pet = new Pet('Luigi');
+      pet.fitness = 4;
+      pet.walk();
+      expect(pet.fitness).toEqual(8);
+    });
+  });
+
+  describe('walk', () => {
+    it('increases fitness by to a maximum of 10', () => {
+      const pet = new Pet('fido');
+      pet.fitness = 8;
+      pet.walk();
+      expect(pet.fitness).toEqual(MAXIMUM_FITNESS);
+    });
+  });
